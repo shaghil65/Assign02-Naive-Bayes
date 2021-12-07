01a545ff866eaed895f023b1f507169e54faf20a
Muhammad Shaghil <shaghil.sha@gmail.com>
2021-12-08 02:53:52 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+#importing libraries
+import pandas as panda
+from sklearn.model_selection import train_test_split
+
+#Getting the whole data set by mentioning its path of drive 
+train=panda.read_csv('/content/drive/MyDrive/smtp/train.csv')
+train.head()
+
+
+#for labels
+y = train.Cover_Type
+
+#for features
+X = train.drop('Cover_Type', axis=1)
+
+#splitting the data set into 80% and 20%
+t_train, t_test, y_train, y_test = train_test_split(X, y,test_size=0.2)
+
+#finally getting the splitting data
+print("\nt_train:\n")
+t_train.head()
+
+print("\nt_test:\n")
+t_test.head()
+
